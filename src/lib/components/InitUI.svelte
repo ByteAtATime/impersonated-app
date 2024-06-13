c1c9e4d9be688d80d9ad5f9500c09e8f731963e6
ByteAtATime <ByteAtATime@proton.me>
2024-06-12 20:45:21 -0700

    fix: change title to "impersonated.app"

--- src/lib/components/InitUI.svelte
+++ src/lib/components/InitUI.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="flex flex-col items-center gap-y-2 justify-center min-h-screen">
-	<h1 class="text-4xl font-bold mb-4">Impersonator.App</h1>
+	<h1 class="text-4xl font-bold mb-4">Impersonated.App</h1>
 	<p class="text-lg">Impersonate any Ethereum address and interact with dApps!</p>
 
 	<p class="text-lg">To get started, enter the address you would like to impersonate:</p>
